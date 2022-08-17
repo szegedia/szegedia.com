2ac50638e0196c32a6e678638718407ebab782b8
Szegedi Attila <rusu@Attilas-MacBook-Pro.local>
2022-08-17 18:40:54 +0200

    ci(astro): update astro deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import image from "@astrojs/image";
 // https://astro.build/config
 export default defineConfig({
   site: "szegedia.github.io",
+  base: "szegedia.com",
   integrations: [tailwind(), image()],
 });
