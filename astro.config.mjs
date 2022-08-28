f91f6adc53702354a2a90c934685cab69882a07a
Szegedi Attila <rusu@Attilas-MacBook-Pro.local>
2022-08-28 18:45:13 +0200

    improve page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-
-import image from "@astrojs/image";
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import react from "@astrojs/react"
+import image from "@astrojs/image"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://szegedia.github.io",
   // base: "/szegedia.com",
-  integrations: [tailwind(), image()],
-});
+  integrations: [react(), tailwind(), image()],
+  syntaxHighlight: "prism"
+})
